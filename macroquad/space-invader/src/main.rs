3762046933bc642bca25354f1423ca5be08989c2
Flint <linus.buchmann@bluewin.ch>
2023-07-31 16:47:13 +0200

    day 7
    im trying to do an atari space invader clone... without tutorial so it would be better if nobody will ever see this

--- macroquad/space-invader/src/main.rs
+++ macroquad/space-invader/src/main.rs
@@ -1,31 +1,22 @@
 use macroquad::prelude::*;
 
-
-
-
-
 #[macroquad::main("Main")]
 async fn main() {
     let mut enemy_x: f32 = 0.0;
     let mut enemy_y: f32 = 0.0;
     let mut player_x: f32 = 0.0;
+    let mut player_y: f32 = 770.0;
     let mut speed: f32 = 14.0;
     let mut collision: bool = false;
     let enemy_texture: Texture2D = load_texture("sprites/enemy.png").await.unwrap();
     let player_texture: Texture2D = load_texture("sprites/pixel-64x64.png").await.unwrap();
-    let player = player_texture;
-    let enemy = enemy_texture;
+
 
     loop {
         clear_background(LIGHTGRAY);
-        draw_texture(&player_texture, player_x, 770., WHITE);
+        draw_texture(&player_texture, player_x, player_y, WHITE);
         draw_texture(&enemy_texture, enemy_x, enemy_y, WHITE);
 
-        if enemy.intersect(&player) {
-            println!("bruuh");
-        }
-
-        next_frame().await;
         if is_key_down(KeyCode::Space) {
             enemy_y += 5.0;
         }
@@ -35,5 +26,6 @@ async fn main() {
         if is_key_down(KeyCode::A) {
             player_x -= speed;
         }
+        next_frame().await
     }
 }
--- macroquad/sprite-collision/src/main.rs
+++ macroquad/sprite-collision/src/main.rs
@@ -1,112 +1,247 @@
 use macroquad::prelude::*;
 
-#[macroquad::main("Arkanoid")]
+const SHIP_HEIGHT: f32 = 25.;
+const SHIP_BASE: f32 = 22.;
+struct Ship {
+    pos: Vec2,
+    rot: f32,
+    vel: Vec2,
+}
+
+struct Bullet {
+    pos: Vec2,
+    vel: Vec2,
+    shot_at: f64,
+    collided: bool,
+}
+
+struct Asteroid {
+    pos: Vec2,
+    vel: Vec2,
+    rot: f32,
+    rot_speed: f32,
+    size: f32,
+    sides: u8,
+    collided: bool,
+}
+
+fn wrap_around(v: &Vec2) -> Vec2 {
+    let mut vr = Vec2::new(v.x, v.y);
+    if vr.x > screen_width() {
+        vr.x = 0.;
+    }
+    if vr.x < 0. {
+        vr.x = screen_width()
+    }
+    if vr.y > screen_height() {
+        vr.y = 0.;
+    }
+    if vr.y < 0. {
+        vr.y = screen_height()
+    }
+    vr
+}
+
+#[macroquad::main("Asteroids")]
 async fn main() {
-    const BLOCKS_W: usize = 10;
-    const BLOCKS_H: usize = 10;
-    const SCR_W: f32 = 20.0;
-    const SCR_H: f32 = 20.0;
+    let mut ship = Ship {
+        pos: Vec2::new(screen_width() / 2., screen_height() / 2.),
+        rot: 0.,
+        vel: Vec2::new(0., 0.),
+    };
 
-    let mut blocks: [[bool; BLOCKS_W]; BLOCKS_H] = [[true; BLOCKS_W]; BLOCKS_H];
-    let mut ball_x = 12.;
-    let mut ball_y = 7.;
-    let mut dx = 3.5;
-    let mut dy = -3.5;
-    let mut platform_x = 10.;
-    let mut stick = true;
-    let platform_width = 5.;
-    let platform_height = 0.2;
+    let mut bullets = Vec::new();
+    let mut last_shot = get_time();
+    let mut asteroids = Vec::new();
+    let mut gameover = false;
 
-    // build camera with following coordinate system:
-    // (0., 0)     .... (SCR_W, 0.)
-    // (0., SCR_H) .... (SCR_W, SCR_H)
-    set_camera(&Camera2D {
-        zoom: vec2(1. / SCR_W * 2., -1. / SCR_H * 2.),
-        target: vec2(SCR_W / 2., SCR_H / 2.),
-        ..Default::default()
-    });
+    let mut screen_center;
 
     loop {
-        clear_background(SKYBLUE);
+        if gameover {
+            clear_background(LIGHTGRAY);
+            let mut text = "You Win!. Press [enter] to play again.";
+            let font_size = 30.;
 
-        let delta = get_frame_time();
-
-        if is_key_down(KeyCode::Right) && platform_x < SCR_W - platform_width / 2. {
-            platform_x += 3.0 * delta;
-        }
-        if is_key_down(KeyCode::Left) && platform_x > platform_width / 2. {
-            platform_x -= 3.0 * delta;
-        }
-
-        if stick == false {
-            ball_x += dx * delta;
-            ball_y += dy * delta;
-        } else {
-            let (font_size, font_scale, font_aspect) = camera_font_scale(1.);
-            let text_params = TextParams {
+            if asteroids.len() > 0 {
+                text = "Game Over. Press [enter] to play again.";
+            }
+            let text_size = measure_text(text, None, font_size as _, 1.0);
+            draw_text(
+                text,
+                screen_width() / 2. - text_size.width / 2.,
+                screen_height() / 2. - text_size.height / 2.,
                 font_size,
-                font_scale,
-                font_scale_aspect: font_aspect,
-                ..Default::default()
-            };
-            draw_text_ex(
-                "Press space to start",
-                SCR_W / 2. - 5.,
-                SCR_H / 2.,
-                text_params,
+                DARKGRAY,
             );
+            if is_key_down(KeyCode::Enter) {
+                ship = Ship {
+                    pos: Vec2::new(screen_width() / 2., screen_height() / 2.),
+                    rot: 0.,
+                    vel: Vec2::new(0., 0.),
+                };
+                bullets = Vec::new();
+                asteroids = Vec::new();
+                gameover = false;
+                screen_center = Vec2::new(screen_width() / 2., screen_height() / 2.);
+                for _ in 0..10 {
+                    asteroids.push(Asteroid {
+                        pos: screen_center
+                            + Vec2::new(rand::gen_range(-1., 1.), rand::gen_range(-1., 1.))
+                                .normalize()
+                                * screen_width().min(screen_height())
+                                / 2.,
+                        vel: Vec2::new(rand::gen_range(-1., 1.), rand::gen_range(-1., 1.)),
+                        rot: 0.,
+                        rot_speed: rand::gen_range(-2., 2.),
+                        size: screen_width().min(screen_height()) / 10.,
+                        sides: rand::gen_range(3, 8),
+                        collided: false,
+                    })
+                }
+            }
+            next_frame().await;
+            continue;
+        }
+        let frame_t = get_time();
+        let rotation = ship.rot.to_radians();
 
-            ball_x = platform_x;
-            ball_y = SCR_H - 0.5;
+        let mut acc = -ship.vel / 100.; // Friction
 
-            stick = !is_key_down(KeyCode::Space);
+        // Forward
+        if is_key_down(KeyCode::Up) {
+            acc = Vec2::new(rotation.sin(), -rotation.cos()) / 3.;
         }
 
-        if ball_x <= 0. || ball_x > SCR_W {
-            dx *= -1.;
-        }
-        if ball_y <= 0.
-            || (ball_y > SCR_H - platform_height - 0.15 / 2.
-                && ball_x >= platform_x - platform_width / 2.
-                && ball_x <= platform_x + platform_width / 2.)
-        {
-            dy *= -1.;
-        }
-        if ball_y >= SCR_H {
-            ball_y = 10.;
-            dy = -dy.abs();
-            stick = true;
+        // Shot
+        if is_key_down(KeyCode::Space) && frame_t - last_shot > 0.5 {
+            let rot_vec = Vec2::new(rotation.sin(), -rotation.cos());
+            bullets.push(Bullet {
+                pos: ship.pos + rot_vec * SHIP_HEIGHT / 2.,
+                vel: rot_vec * 7.,
+                shot_at: frame_t,
+                collided: false,
+            });
+            last_shot = frame_t;
         }
 
-        for j in 0..BLOCKS_H {
-            for i in 0..BLOCKS_W {
-                if blocks[j][i] {
-                    let block_w = SCR_W / BLOCKS_W as f32;
-                    let block_h = 7.0 / BLOCKS_H as f32;
-                    let block_x = i as f32 * block_w + 0.05;
-                    let block_y = j as f32 * block_h + 0.05;
+        // Steer
+        if is_key_down(KeyCode::Right) {
+            ship.rot += 5.;
+        } else if is_key_down(KeyCode::Left) {
+            ship.rot -= 5.;
+        }
 
-                    draw_rectangle(block_x, block_y, block_w - 0.1, block_h - 0.1, DARKBLUE);
-                    if ball_x >= block_x
-                        && ball_x < block_x + block_w
-                        && ball_y >= block_y
-                        && ball_y < block_y + block_h
-                    {
-                        dy *= -1.;
-                        blocks[j][i] = false;
+        // Euler integration
+        ship.vel += acc;
+        if ship.vel.length() > 5. {
+            ship.vel = ship.vel.normalize() * 5.;
+        }
+        ship.pos += ship.vel;
+        ship.pos = wrap_around(&ship.pos);
+
+        // Move each bullet
+        for bullet in bullets.iter_mut() {
+            bullet.pos += bullet.vel;
+        }
+
+        // Move each asteroid
+        for asteroid in asteroids.iter_mut() {
+            asteroid.pos += asteroid.vel;
+            asteroid.pos = wrap_around(&asteroid.pos);
+            asteroid.rot += asteroid.rot_speed;
+        }
+
+        // Bullet lifetime
+        bullets.retain(|bullet| bullet.shot_at + 1.5 > frame_t);
+
+        let mut new_asteroids = Vec::new();
+        for asteroid in asteroids.iter_mut() {
+            // Asteroid/ship collision
+            if (asteroid.pos - ship.pos).length() < asteroid.size + SHIP_HEIGHT / 3. {
+                gameover = true;
+                break;
+            }
+
+            // Asteroid/bullet collision
+            for bullet in bullets.iter_mut() {
+                if (asteroid.pos - bullet.pos).length() < asteroid.size {
+                    asteroid.collided = true;
+                    bullet.collided = true;
+
+                    // Break the asteroid
+                    if asteroid.sides > 3 {
+                        new_asteroids.push(Asteroid {
+                            pos: asteroid.pos,
+                            vel: Vec2::new(bullet.vel.y, -bullet.vel.x).normalize()
+                                * rand::gen_range(1., 3.),
+                            rot: rand::gen_range(0., 360.),
+                            rot_speed: rand::gen_range(-2., 2.),
+                            size: asteroid.size * 0.8,
+                            sides: asteroid.sides - 1,
+                            collided: false,
+                        });
+                        new_asteroids.push(Asteroid {
+                            pos: asteroid.pos,
+                            vel: Vec2::new(-bullet.vel.y, bullet.vel.x).normalize()
+                                * rand::gen_range(1., 3.),
+                            rot: rand::gen_range(0., 360.),
+                            rot_speed: rand::gen_range(-2., 2.),
+                            size: asteroid.size * 0.8,
+                            sides: asteroid.sides - 1,
+                            collided: false,
+                        })
                     }
+                    break;
                 }
             }
         }
 
-        draw_circle(ball_x, ball_y, 0.2, RED);
-        draw_rectangle(
-            platform_x - platform_width / 2.,
-            SCR_H - platform_height,
-            platform_width,
-            platform_height,
-            DARKPURPLE,
+        // Remove the collided objects
+        bullets.retain(|bullet| bullet.shot_at + 1.5 > frame_t && !bullet.collided);
+        asteroids.retain(|asteroid| !asteroid.collided);
+        asteroids.append(&mut new_asteroids);
+
+        // You win?
+        if asteroids.len() == 0 {
+            gameover = true;
+        }
+
+        if gameover {
+            continue;
+        }
+
+        clear_background(LIGHTGRAY);
+
+        for bullet in bullets.iter() {
+            draw_circle(bullet.pos.x, bullet.pos.y, 2., BLACK);
+        }
+
+        for asteroid in asteroids.iter() {
+            draw_poly_lines(
+                asteroid.pos.x,
+                asteroid.pos.y,
+                asteroid.sides,
+                asteroid.size,
+                asteroid.rot,
+                2.,
+                BLACK,
+            )
+        }
+
+        let v1 = Vec2::new(
+            ship.pos.x + rotation.sin() * SHIP_HEIGHT / 2.,
+            ship.pos.y - rotation.cos() * SHIP_HEIGHT / 2.,
         );
+        let v2 = Vec2::new(
+            ship.pos.x - rotation.cos() * SHIP_BASE / 2. - rotation.sin() * SHIP_HEIGHT / 2.,
+            ship.pos.y - rotation.sin() * SHIP_BASE / 2. + rotation.cos() * SHIP_HEIGHT / 2.,
+        );
+        let v3 = Vec2::new(
+            ship.pos.x + rotation.cos() * SHIP_BASE / 2. - rotation.sin() * SHIP_HEIGHT / 2.,
+            ship.pos.y + rotation.sin() * SHIP_BASE / 2. + rotation.cos() * SHIP_HEIGHT / 2.,
+        );
+        draw_triangle_lines(v1, v2, v3, 2., BLACK);
 
         next_frame().await
     }
