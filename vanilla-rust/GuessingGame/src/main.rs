3f2b3c910f8ebb4b06f4a348ce46eec06cf154c6
Flint <linus.buchmann@bluewin.ch>
2023-08-21 21:59:05 +0200

    day 12

--- vanilla-rust/GuessingGame/src/main.rs
+++ vanilla-rust/GuessingGame/src/main.rs
@@ -1,17 +1,21 @@
 use std::io;
 use rand::Rng;
 
-
 fn main() {
-    let random_number = rand::thread_rng().gen_range(1..20);
-    let mut guessed_number: String = String::new();
-    let random_number_string=random_number.to_string();
-    io::stdin().read_line(&mut guessed_number).expect("fuck you");
-    if random_number_string == guessed_number.trim()
-        { print!("You got it right\n"); }
-    else {
-        println!("the right answer was: {}, you guessed {}", random_number, guessed_number);
+    println!("try to guess the random number");
+    loop {
+        let random_number = rand::thread_rng().gen_range(1..20);
+        let mut guessed_number: String = String::new();
+        let random_number_string=random_number.to_string();
+        io::stdin().read_line(&mut guessed_number).expect("fuck you");
+        if random_number_string != guessed_number.trim()
+            {
+            println!("the right answer was: {}, you guessed {}", random_number, guessed_number);
+            println!("try again");
+            }
+        else {
+            { print!("You got it right\n"); }
+            break;
+        }
     }
-
-    
 }
